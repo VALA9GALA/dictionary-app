d91ca10fc7f730b6147dcc6921eac0293cad0ce1
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-03-13 18:36:44 -0400

    testing bootstrap, link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ export default function App() {
       <header className="App-header">
         <h1>Dictionary App</h1>
       </header>
-      <a href="https://valagala.netlify.app" className="btn btn-info">
+      <a
+        href="https://valagala.netlify.app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn btn-info"
+      >
         Test
       </a>
     </div>
