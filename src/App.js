c850f5e8d945a029a1c99b7eeb10ad7640ff3ab4
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-03-21 19:05:31 -0400

    set up search engine

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary App</h1>
-      </header>
-      <a
-        href="https://valagala.netlify.app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="btn btn-info"
-      >
-        Test
-      </a>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            Coded by{" "}
+            <a
+              href="https://valagala.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              VALAGALA
+            </a>{" "}
+            See{" "}
+            <a
+              href="https://github.com/VALA9GALA/dictionary-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              HERE
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
