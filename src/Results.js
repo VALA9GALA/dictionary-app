01c9f05f3bf903a20ac68e1b7d73b59f65e5ac0a
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-03-31 22:03:09 -0400

    added audio icon
    linked the icon to the audio file url

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,30 @@
 import React from "react";
 import Meaning from "./Meaning";
 
+import "./Dictionary.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophoneLines } from "@fortawesome/free-solid-svg-icons";
+
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>
           <span className="word">{props.results.word}</span>
+          {" "}
           <span>{props.results.phonetic}</span>
+          <span>
+            {" "}
+            <a
+              className="audioIcon"
+              href={props.results.phonetics[0].audio}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faMicrophoneLines} />
+            </a>
+          </span>
         </h2>
         {props.results.meanings.map(function (meaning, index) {
           return (
